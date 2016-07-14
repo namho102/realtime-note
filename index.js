4e26aee3765f051d7b2e8a0cdfc938b986c14e99
Hoang Nam <namhoang.td09@gmail.com>
2016-07-14 15:29:32 +0700

    add socket and rethinkdb

--- index.js
+++ index.js
@@ -1,15 +1,40 @@
-var express = require('express');
 var path = require('path');
+
+var express = require('express');
 var app = express();
 
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+var r = require('rethinkdb');
+
+r.connect({
+	db: 'Messenger'
+}).then(function(connection) {
+
+	io.on('connection', function(socket) {
+		console.log('a user connected');
+		socket.on('disconnect', function() {
+			console.log('user disconnected');
+		});
+
+		socket.on('content', function(data) {
+			// console.log(data);
+			r.table("Notes").update({content: data}).run(connection);
+			
+		})
+	});
+
+})
 app.set('port', (process.env.PORT || 8000));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function(req, res) {
-  res.sendFile(__dirname + '/index.html');
+	res.sendFile(__dirname + '/index.html');
 });
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+
+http.listen(app.get('port'), function() {
+	console.log('Node app is running on port', app.get('port'));
 });

cb2c2f2989f44ae3635b712fe51b2e32bfc98330
Hoang Nam <namhoang.td09@gmail.com>
2016-07-20 08:28:42 +0700

    add socket room

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+var os = require('os');
 var path = require('path');
 
 var express = require('express');
@@ -7,6 +8,18 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 var r = require('rethinkdb');
+
+app.set('port', (process.env.PORT || 8000));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', function(req, res) {
+	res.sendFile(__dirname + '/index.html');
+});
+
+http.listen(app.get('port'), function() {
+	console.log('Node app is running on port', app.get('port'));
+});
 
 r.connect({
 	db: 'Messenger'
@@ -32,18 +45,62 @@ r.connect({
 			r.table("Notes").update({content: data}).run(connection);
 
 		})
+
+		// convenience function to log server messages on the client
+	  function log() {
+	    var array = ['Message from server:'];
+	    array.push.apply(array, arguments);
+	    socket.emit('log', array);
+	  }
+
+	  socket.on('message', function(message) {
+	    log('Client said: ', message);
+	    // for a real app, would be room-only (not broadcast)
+	    socket.broadcast.emit('message', message);
+	  });
+
+	  socket.on('create or join', function(room) {
+
+	    log('Received request to create or join room ' + room);
+
+	    // var numClients = io.sockets.sockets.length;
+			var numClients = Object.keys(io.sockets.sockets).length;
+
+	    log('Room ' + room + ' now has ' + numClients + ' client(s)');
+
+			// console.log(io.sockets)
+
+	    if (numClients === 1) {
+	      socket.join(room);
+	      log('Client ID ' + socket.id + ' created room ' + room);
+	      socket.emit('created', room, socket.id);
+
+	    } else if (numClients === 2) {
+	      log('Client ID ' + socket.id + ' joined room ' + room);
+	      io.sockets.in(room).emit('join', room);
+	      socket.join(room);
+	      socket.emit('joined', room, socket.id);
+	      io.sockets.in(room).emit('ready');
+	    } else { // max two clients
+	      socket.emit('full', room);
+	    }
+	  });
+
+	  socket.on('ipaddr', function() {
+	    var ifaces = os.networkInterfaces();
+	    for (var dev in ifaces) {
+	      ifaces[dev].forEach(function(details) {
+	        if (details.family === 'IPv4' && details.address !== '127.0.0.1') {
+	          socket.emit('ipaddr', details.address);
+	        }
+	      });
+	    }
+	  });
+
+	  socket.on('bye', function(){
+	    console.log('received bye');
+	  });
+
 	});
 
-})
-app.set('port', (process.env.PORT || 8000));
-
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.get('/', function(req, res) {
-	res.sendFile(__dirname + '/index.html');
-});
-
-
-http.listen(app.get('port'), function() {
-	console.log('Node app is running on port', app.get('port'));
 });
